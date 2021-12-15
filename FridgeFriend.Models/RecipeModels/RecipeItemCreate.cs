eef627bccb7ad18e86b71b286a0d8d768b9bf7f0
Rex Meyer <armeyer1998@gmail.com>
2021-12-14 20:02:13 -0500

    recipe item data added

---
+++ FridgeFriend.Models/RecipeModels/RecipeItemCreate.cs
@@ -0,0 +1,29 @@
+﻿using FridgeFriend.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeFriend.Models.RecipeModels
+{
+    public class RecipeItemCreate
+    {
+        public int ItemID { get; set; }
+        [MinLength(5, ErrorMessage = "Please enter at least 5 characters.")]
+        [MaxLength(25, ErrorMessage = "Too many characters. Max is 25.")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Calories { get; set; }
+        [Required]
+        [Display(Name = "Date Purchased")]
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        [Display(Name = "Date Expired")]
+        public DateTime ExpirationDate { get; set; }
+        [Required]
+        public FoodGroup Type { get; set; }
+    }
+}
--- FridgeFriend.WebApi/Providers/ApplicationOAuthProvider.cs
+++ FridgeFriend.WebApi/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using FridgeFriend.WebApi.Models;
+using FridgeFriend.Data;
 
 namespace FridgeFriend.WebApi.Providers
 {
