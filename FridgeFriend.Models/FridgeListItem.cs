44cc522a13e97ddadd1bcfaee9249cbdacd6db3e
Danny Redden <dannyredden18@gmail.com>
2021-12-21 14:57:22 -0500

    created fridgecreate class

--- FridgeFriend.Models/FridgeListItem.cs
+++ FridgeFriend.Models/FridgeListItem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FridgeFriend.Models
 {
-    class FridgeListItem
+    public class FridgeListItem
     {
+        public int FridgeId { get; set; }
+        public string FridgeName { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
